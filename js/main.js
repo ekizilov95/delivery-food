352f0c9a95b7f6483f014a82f50ea1a5b40ec793
Evgen <e.kizilov95@gmail.com>
2020-10-30 15:58:56 +0300

    third commit

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ function notAuthorized() {
   buttonAuth.addEventListener('click', openModal);
   closeAuth.addEventListener('click', closeModal);
   loginInForm.classList.remove('hide');
-  
+
 }
 
 function authorized() {
@@ -97,77 +97,121 @@ notAuthorized();
 // — Если пользователь не авторизован и кликнет на карточку то открываться должно модальное окно с авторизацией
 
 const cardsRestaurants = document.querySelector('.cards-restaurants'),
-  promo = document.querySelector('.container-promo'),
+  promo = document.querySelector('.swiper-container'),
   restaurants = document.querySelector('.restaurants'),
   menu = document.querySelector('.menu'),
   logo = document.querySelector('.logo'),
-  cardsMenu = document.querySelector('.cards-menu');
+  cardsMenu = document.querySelector('.cards-menu'),
+  menuHeading = document.querySelector('.menu-heading'),
+  preloader = document.createElement('img'),
+  restaurantTitle = document.querySelector('.restaurant-title'),
+  restauranRating = document.querySelector('.rating'),
+  restaurantPrice = document.querySelector('.price'),
+  restaurantCategory = document.querySelector('.category');
 
 
+  function createCardsRestaurant({
+    image,
+    kitchen,
+    name,
+    price,
+    products,
+    stars,
+    time_of_delivery: timeOfDelivery
+  }) {
 
-function createCardsRestaurant() {
-
-  const card = `
-      <a class="card card-restaurant">
-        <img src="img/pizza-plus/preview.jpg" alt="image" class="card-image"/>
+    const cardRestaurant = document.createElement('a');
+    cardRestaurant.className = 'card card-restaurant';
+    cardRestaurant.products = products;
+    cardRestaurant.info = {name, price, products, stars, kitchen};
+    const card = `
+        <img src="${image}" alt="image" class="card-image"/>
         <div class="card-text">
           <div class="card-heading">
-            <h3 class="card-title">Пицца плюс</h3>
-            <span class="card-tag tag">50 мин</span>
+            <h3 class="card-title">${name}</h3>
+            <span class="card-tag tag">${timeOfDelivery} мин</span>
           </div>
-          <!-- /.card-heading -->
           <div class="card-info">
             <div class="rating">
-              4.5
+            ${stars}
             </div>
-            <div class="price">От 900 ₽</div>
-            <div class="category">Пицца</div>
+            <div class="price">От ${price} ₽</div>
+            <div class="category">${kitchen}</div>
           </div>
         </div>
-      </a>
     `;
-  cardsRestaurants.insertAdjacentHTML('beforeend', card);
-}
+    cardRestaurant.insertAdjacentHTML('beforeend', card);
+    cardsRestaurants.insertAdjacentElement('beforeend', cardRestaurant);
+  }
 
 function linkToRestaurant(e) {
   let target = e.target.closest('.card-restaurant');
+  cardsRestaurants.style.opacity = '0.5';
+  preloader.src = 'img/original.svg';
+  preloader.style.cssText = `
+    position:absolute;
+    content: "";
+    top: 46%;
+    left: 50%;
+    transform: scale(2);
+  `;
+  cardsRestaurants.append(preloader);
 
-  if (target) {
-    if(login) {
-      promo.classList.add('hide');
-      restaurants.classList.add('hide');
-      menu.classList.remove('hide');
-    } else {
-      openModal();
+  setTimeout(() => {
+    if (target) {
+      if (login) {
+        promo.classList.add('hide');
+        restaurants.classList.add('hide');
+        menu.classList.remove('hide');
+        cardsMenu.textContent = '';
+
+        const {name, stars, price, kitchen} = target.info;
+        console.dir(target.info);
+        restaurantTitle.textContent = name;
+        restauranRating.textContent = stars;
+        restaurantPrice.textContent = `От ${price} p`;
+        restaurantCategory.textContent = kitchen;
+
+        getData(`./db/${target.products}`)
+          .then(data => {
+            data.forEach(createCardsMenu);
+          });
+      } else {
+        openModal(createCardsMenu);
+      }
     }
- 
-  } 
+  }, 800);
 
 }
 
-function createCardsMenu() {
+function createCardsMenu({
+  description,
+  id,
+  image,
+  name,
+  price
+}) {
+
   const card = document.createElement('div');
   card.className = 'card';
   card.innerHTML = `
-    <img src="img/pizza-plus/pizza-vesuvius.jpg" alt="image" class="card-image" />
+    <img src="${image}" alt="image" class="card-image"/>
     <div class="card-text">
       <div class="card-heading">
-        <h3 class="card-title card-title-reg">Пицца Везувий</h3>
+        <h3 class="card-title card-title-reg">${name}</h3>
       </div>
-      <!-- /.card-heading -->
       <div class="card-info">
-        <div class="ingredients">Соус томатный, сыр «Моцарелла», ветчина, пепперони, перец
-          «Халапенье», соус «Тобаско», томаты.
+        <div class="ingredients">${description}
         </div>
       </div>
-      <!-- /.card-info -->
       <div class="card-buttons">
         <button class="button button-primary button-add-cart">
           <span class="button-card-text">В корзину</span>
           <span class="button-cart-svg"></span>
         </button>
-        <strong class="card-price-bold">545 ₽</strong>
+        <strong class="card-price-bold">${price} ₽</strong>
       </div>
+    </div>
   `;
   cardsMenu.append(card);
 
@@ -179,13 +223,51 @@ logo.addEventListener('click', () => {
   promo.classList.remove('hide');
   restaurants.classList.remove('hide');
   menu.classList.add('hide');
+  cardsRestaurants.style.opacity = '';
+  preloader.style.display = "none";
 });
 
-createCardsMenu();
-createCardsMenu();
-createCardsMenu();
-createCardsMenu();
 
-createCardsRestaurant();
-createCardsRestaurant();
-createCardsRestaurant();
+
+// day 3
+// slider
+
+const mySwiper = new Swiper('.swiper-container', {
+  sliderPerView: 1,
+  loop: true,
+  autoplay: true,
+  effect: 'cube',
+  speed: 800,
+  cubeEffect: {
+    shadow: false,
+
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+});
+
+//third module
+
+const getData = async function (url) {
+
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`Could not feth ${url}, status: ${response.status}`);
+  }
+
+  return await response.json();
+
+}
+
+getData("./db/partners.json")
+  .then(data => {
+    data.forEach(createCardsRestaurant);
+  });
